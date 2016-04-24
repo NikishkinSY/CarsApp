3017bdbfd8f3e1a3ad763b04004454639efd8177
Sergey <frodo89@nm.ru>
2016-04-24 21:48:41 +0300

    - disable delete via creating new item

--- CarsApp/src/CarsApp/wwwroot/js/app/modules/core/confirm.directive.js
+++ CarsApp/src/CarsApp/wwwroot/js/app/modules/core/confirm.directive.js
@@ -3,18 +3,21 @@
 
     angular
         .module('app.core')
-        .directive('ngConfirmClick', [
-            function () {
-                return {
-                    link: function (scope, element, attr) {
-                        var msg = attr.ngConfirmClick || "Are you sure?";
-                        var clickAction = attr.confirmedClick;
-                        element.bind('click', function (event) {
-                            if (window.confirm(msg)) {
-                                scope.$eval(clickAction)
-                            }
-                        });
+        .directive('ngConfirmClick', Confirm)
+        
+    Confirm.$inject = [];
+
+    function Confirm() {
+        return {
+            link: function (scope, element, attr) {
+                var msg = attr.ngConfirmClick || "Are you sure?";
+                var clickAction = attr.confirmedClick;
+                element.bind('click', function (event) {
+                    if (window.confirm(msg)) {
+                        scope.$eval(clickAction)
                     }
-                };
-            }])
+                });
+            }
+        };
+    }
 })();
